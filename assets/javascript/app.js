c639df0cba5c3c8b5f9d9b3f31de0a75e2732cba
Kelsey Tobin <etobinkelsey@gmail.com>
2019-01-13 13:23:16 -0700

    commented pseudo js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+//initialize firebase through var config and initialize command
+
+//create click event for submit button
+
+//retrieve user input from form within click event
+
+//create object to hold train data that will be stored in firebase
+
+//upload train data to firebase
+
+//console log to make sure this is working
+
+//clear text boxes in form
+
+//end click event
+
+//create firebase event for addding new train to database and to html table via new row
+
+    //store all input values into variable
+
+    //log variables to make sure values are being grabbed
+
+    //reformat train start time 
+
+    //calcuclate difference in minutes between train start time, last train, and next arriving
+
+    //create new table row
+
+    //append new table row to html
