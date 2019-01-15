bdefee1941c1d4593390c630a49300f47d828bea
Kelsey Tobin <etobinkelsey@gmail.com>
2019-01-14 21:28:15 -0700

    reformated firebase event variables, user input displaying to table, minutes away calculation not working, new firebase database linked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,38 @@
-//initialize firebase through var config and initialize command
+// Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDLHC5LRdN5p3CWPYXTcgmwJEobOo5_hec",
-    authDomain: "train-scheduling-app-d7fe2.firebaseapp.com",
-    databaseURL: "https://train-scheduling-app-d7fe2.firebaseio.com",
-    projectId: "train-scheduling-app-d7fe2",
-    storageBucket: "train-scheduling-app-d7fe2.appspot.com",
-    messagingSenderId: "1095971288978"
+    apiKey: "AIzaSyB-rKqiTcCUdBOmP8F3pQPyur_KN7pZ_XE",
+    authDomain: "train-app-c8ef2.firebaseapp.com",
+    databaseURL: "https://train-app-c8ef2.firebaseio.com",
+    projectId: "train-app-c8ef2",
+    storageBucket: "",
+    messagingSenderId: "8830646799"
   };
-  
   firebase.initializeApp(config);
 
-//create var to hold firebase
+  //create var to hold firebase
 var database = firebase.database();
+
+var trainName = "";
+var trainDestination = "";
+var firstTime = "";
+var trainFrequency = "";
 
 //create click event for submit button
 $("#add-train-btn").on("click", function(event) {
     event.preventDefault(); //keep form from resubmitting entire page
 
     //retrieve user input from form within click event
-    var trainName = $("#train-name-input").val().trim();
-    var trainDestination = $("#destination-input").val().trim();
-    var firstTime = $("#first-train-input").val().trim();
-    var trainFrequency = $("#frequency-input").val().trim();
+    tName = $("#train-name-input").val().trim();
+    tDestination = $("#destination-input").val().trim();
+    tTime = moment($("#first-train-input").val().trim(),  "HH:mm").format("X");
+    tFreq = $("#frequency-input").val().trim();
 
     //create object to hold train data that will be stored in firebase
     var newTrain = {
-        name: trainName,
-        destination: trainDestination,
-        first: firstTime,
-        frequency: trainFrequency
+        name: tName,
+        destination: tDestination,
+        first: tTime,
+        frequency: tFreq
     };
 
     //upload train data to firebase
@@ -52,26 +56,30 @@ $("#add-train-btn").on("click", function(event) {
 //create firebase event for addding new train to database and to html table via new row
 database.ref().on("child_added", function(childSnapshot) {
     console.log(childSnapshot.val());
+    
 
     //store all input values into variable
-    var trainName = childSnapshot.val().name;
-    var trainDestination =  childSnapshot.val().destination;
-    var firstTime = childSnapshot.val().first;
-    var trainFrequency = childSnapshot.val().frequency;
+    var newName = childSnapshot.val().name;
+    var newDestination =  childSnapshot.val().destination;
+    var newTime = childSnapshot.val().tTime;
+    var newFreq = childSnapshot.val().frequency;
 
     //log variables to make sure values are being grabbed
-    console.log(name);
-    console.log(destination);
-    console.log(first);
-    console.log(frequency);
+    console.log(newName);
+    console.log(newDestination);
+    console.log(newTime);
+    console.log(newFreq);
 
     // ----------- HOMEWORK CODE STARTS HERE- CALCULATE ARRIVAL AND MINUTES AWAY----------------//
     
     //reformat train start time, create var to hold next arrival
- 
+    var tReformat = moment.unix(newTime).format("HH:mm");
 
-    //calcuclate difference in minutes between train start time, last train, and next arriving
-    //create var to hold minutes away
+     // Frequency
+     var frequency = newFreq;
+
+     // New Time
+     var firstTime = newTime;
     
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
@@ -86,11 +94,11 @@ database.ref().on("child_added", function(childSnapshot) {
     console.log("DIFFERENCE IN TIME: " + diffTime);
 
     // Time apart (remainder)
-    var tRemainder = diffTime % trainFrequency;
+    var tRemainder = diffTime % frequency;
     console.log(tRemainder);
 
     // Minute Until Train
-    var minutesAway = trainFrequency - tRemainder;
+    var minutesAway = frequency - tRemainder;
     console.log("MINUTES TILL TRAIN: " + minutesAway);
 
     // Next Train
@@ -99,15 +107,17 @@ database.ref().on("child_added", function(childSnapshot) {
 
     //create new table row
     var newRow = $("<tr>").append(
-        $("<td>").text(trainName),
-        $("<td>").text(trainDestination),
-        $("<td>").text(trainFrequency),
+        $("<td>").text(newName),
+        $("<td>").text(newDestination),
+        $("<td>").text(newFreq),
         $("<td>").text(nextTrain),
         $("<td>").text(minutesAway)
       );
 
     //append new table row to html
     $("#train-table > tbody").append(newRow);
+
+    // Run error function if it doesnt work
 
 //end firebase event
 });
