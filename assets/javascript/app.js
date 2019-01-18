389b66ec357b8e653374d070bd595b3e3d4d2090
Kelsey Tobin <etobinkelsey@gmail.com>
2019-01-17 18:10:15 -0700

    minute calculation working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,25 +6,21 @@ var config = {
     projectId: "train-app-c8ef2",
     storageBucket: "",
     messagingSenderId: "8830646799"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  //create var to hold firebase
+//create var to hold firebase
 var database = firebase.database();
 
-var trainName = "";
-var trainDestination = "";
-var firstTime = "";
-var trainFrequency = "";
 
 //create click event for submit button
-$("#add-train-btn").on("click", function(event) {
+$("#add-train-btn").on("click", function (event) {
     event.preventDefault(); //keep form from resubmitting entire page
 
     //retrieve user input from form within click event
     tName = $("#train-name-input").val().trim();
     tDestination = $("#destination-input").val().trim();
-    tTime = moment($("#first-train-input").val().trim(),  "HH:mm").format("X");
+    tTime = moment($("#first-train-input").val().trim(), "HH:mm").format("X");
     tFreq = $("#frequency-input").val().trim();
 
     //create object to hold train data that will be stored in firebase
@@ -44,25 +40,27 @@ $("#add-train-btn").on("click", function(event) {
     console.log(newTrain.first);
     console.log(newTrain.frequency);
 
+
     //clear text boxes in form
     $("#train-name-input").val("");
     $("#destination-input").val("");
     $("#first-train-input").val("");
     $("#frequency-input").val("");
 
-//end click event
+    //end click event
 });
 
 //create firebase event for addding new train to database and to html table via new row
-database.ref().on("child_added", function(childSnapshot) {
+database.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val());
-    
+
 
     //store all input values into variable
     var newName = childSnapshot.val().name;
-    var newDestination =  childSnapshot.val().destination;
-    var newTime = childSnapshot.val().tTime;
+    var newDestination = childSnapshot.val().destination;
+    var newTime = childSnapshot.val().first;
     var newFreq = childSnapshot.val().frequency;
+
 
     //log variables to make sure values are being grabbed
     console.log(newName);
@@ -71,16 +69,16 @@ database.ref().on("child_added", function(childSnapshot) {
     console.log(newFreq);
 
     // ----------- HOMEWORK CODE STARTS HERE- CALCULATE ARRIVAL AND MINUTES AWAY----------------//
-    
+
     //reformat train start time, create var to hold next arrival
-    var tReformat = moment.unix(newTime).format("HH:mm");
+    // var tReformat = moment.unix(newTime).format("HH:mm");
 
-     // Frequency
-     var frequency = newFreq;
+    // Frequency
+    var frequency = newFreq;
 
-     // New Time
-     var firstTime = newTime;
-    
+    // New Time
+    var firstTime = newTime;
+
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
     console.log(firstTimeConverted);
@@ -90,6 +88,7 @@ database.ref().on("child_added", function(childSnapshot) {
     console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
     // Difference between the times
+    
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
     console.log("DIFFERENCE IN TIME: " + diffTime);
 
@@ -103,29 +102,33 @@ database.ref().on("child_added", function(childSnapshot) {
 
     // Next Train
     var nextTrain = moment().add(minutesAway, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm A"));
 
-   
+
     //create new table row
     var newRow = $("<tr>").append(
-            $("<td>").text(newName),
-            $("<td>").text(newDestination),
-            $("<td>").text(newFreq),
-            $("<td>").text(nextTrain),
-            $("<td>").text(minutesAway),
-            $("<button>").attr("id", "remove-train-btn").text("Remove")
-      );
+        $("<td>").text(newName),
+        $("<td>").text(newDestination),
+        $("<td>").text(newFreq),
+        $("<td>").text(nextTrain),
+        $("<td>").text(minutesAway),
+        $("<button>").attr("id", "remove-train-btn").text("Remove"),
+    );
 
     //append new table row to html
     $("#train-table > tbody").append(newRow);
 
-  
+
+
+    //create remove train button event
+    // $("#remove-train-btn").on("click", function() { //accessing button through id assigned in append new row
+    //     var table = $("#train-table");
+    //     var row = table.rows[index];
+    //     console.log(row.id);
+
+    // });
+
     // Run error function if it doesnt work
 
-//end firebase event
+    //end firebase event
 });
-
-//create firebase event for user being able to delete train row from html table
-// $("#remove-train-btn").on("click", function() {
-//     console.log("id");
-// });
